cbfdaecc1b1385ceeb8d43803d8b73e6add4db2d
Proxy <69701744+Proxylol@users.noreply.github.com>
2024-12-25 06:21:21 -0500

    optimized code
    noticed it was using a lot of the ram in my vps, so i tried to do some optimizing

--- self.py
+++ self.py
@@ -1,42 +1,30 @@
 from discum.utils.slash import SlashCommander
 import discum
 import time
-import threading
-import random
 
-TOKEN = "" # Account token
-GUILD_ID = "" # Server ID
-CHANNEL_ID = "" # Channel ID
+TOKEN = "" # selfbot token
+GUILD_ID = "" # server ID
+CHANNEL_ID = "" # channel ID (to send bump)
 
 bot = discum.Client(token=TOKEN, log=False)
 
-def send_bump_command(): # This just makes sure the bot does the command as soon as you turn it on.
-    try:
-        slashCmds = bot.getSlashCommands("302050872383242240").json()
-        s = SlashCommander(slashCmds)
-        data = s.get(['bump'])
-        bot.triggerSlashCommand("302050872383242240", CHANNEL_ID, guildID=GUILD_ID, data=data)
-        print('Sent initial bump')
-    except Exception as e:
-        print(f"Error occurred sending initial bump: {e}")
-    
+try:
+    slashCmds = bot.getSlashCommands("302050872383242240").json()
+    s = SlashCommander(slashCmds)
+    data = s.get(['bump'])
+except Exception as e:
+    print(f"couldnt find the slash command, make sure to send a message to the Disboard bot first")
+    exit()
+
+def send_bump_command():
+    """auto bump"""
     while True:
-        random_sleep_time = random.uniform(300, 400) # Does the command every 5-6 minutes, EDIT IF YOU WANT. (300 seconds = 5mins)
-        print(f"Waiting {random_sleep_time:.2f} seconds.")
-        time.sleep(random_sleep_time)
-        
         try:
-            slashCmds = bot.getSlashCommands("302050872383242240").json()
-            s = SlashCommander(slashCmds)
-            data = s.get(['bump'])
-            
             bot.triggerSlashCommand("302050872383242240", CHANNEL_ID, guildID=GUILD_ID, data=data)
-            print('Sent bump')
-        
+            print("bumped")
         except Exception as e:
-            print(f"An error occurred: {e}")
+            print(f"error: {e}")
+        time.sleep(7520) # you can change this to whatever, this will do it every 2 hours and 2 minutes
 
-thread = threading.Thread(target=send_bump_command)
-thread.start()
-
-bot.gateway.run(auto_reconnect=True)
+print("starting")
+send_bump_command()
